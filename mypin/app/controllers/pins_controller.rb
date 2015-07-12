cfaea3a27aca1d3b232143f469cb327ffb004f61
andy-dev <andres.macedo@gmail.com>
2015-07-11 17:21:14 -0700

    add basic methods pin controller, add views for new pins and simple form using simple form gem

--- mypin/app/controllers/pins_controller.rb
+++ mypin/app/controllers/pins_controller.rb
@@ -1,4 +1,17 @@
 class PinsController < ApplicationController
   def index
   end
+
+  def new
+    @pin = Pin.new
+  end
+
+  def create
+    @pin = Pin.new(pin_params)
+  end
+
+  private
+  def pin_params
+    params.require(:pin)(:title, :description)
+  end
 end
