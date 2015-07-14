6cba43d43378a99775a4c57889b957164a25eee7
andy-dev <andres.macedo@gmail.com>
2015-07-14 15:00:17 -0700

    add basic test, testing is in wip

--- mypin/spec/controllers/homepage_controller_spec.rb
+++ mypin/spec/controllers/homepage_controller_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe PinsController, type: :controller do
-  it 'should go ' do
+  it 'should render show page ' do
     get :show
+    expect(response).to have_rendered('/:id')
   end
 end
